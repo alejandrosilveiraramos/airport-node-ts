4953d98a4ccb04905ff8cc521ceb384561773377
alejandrosilveiraramos <alejandrosilveiraramos3@gmail.com>
2023-03-07 13:58:24 -0300

    feat: Starts configs, Database config, Model: Airports. Starting Controller: Airports

---
+++ src/config/databaseExemple.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { Airport } from '../model/airports';
+
+const AppDataSource = new DataSource({
+    type: 'oracle',
+    host: '0',
+    port: 1521,
+    username: '0',
+    password: '0',
+    database: 'xe',
+    entities: [Airport],
+    migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
+});
+
+export default AppDataSource;
